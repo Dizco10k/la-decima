1d5169d316b55092f46a169e69ac044ad7a78fcd
Aleixer Alvarado Bernal <aleixer.alvarado@softtek.com>
2024-07-29 20:12:33 -0500

    Banner

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Header from './components/Header'
 import ProductList from './components/ProductList';
 import Footer from './components/Footer';
+import Banner from './components/Banner';
+
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Banner/>
       <main>
         <ProductList />
       </main>
