504151f998630a3ce310ab6550ff3e4eb457ad29
@Dizko10k <diezk.10k@gmail.com>
2024-07-30 08:15:58 -0500

    ajusteheader

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,18 @@ import './header.css';
 function Header() {
     return (
         <header className="header">
-            <img src="Logo" alt="Logo" />
+            <img className="logo" src="logo.webp" alt="Logo" />
             <nav>               
                 <ul>
                     <li>Inicio</li>
-                    <li>Inicio</li>
+                    <li>Contactanos</li>
+                    <li>Productos</li>
+                    <li>Ofertas</li>
+                    <li>
+                        <img className="carrito" src="carrito2.svg" alt="Logo" />
+                    </li>
                 </ul>
+                
             </nav>
         </header>
     )
